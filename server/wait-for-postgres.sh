30c1a643c09868536f92980e15c5f3fa9f0b6516
Milad Nozari <mnvoh90@gmail.com>
2018-07-18 23:22:43 +0430

    Add a script to wait for postgres

---
+++ server/wait-for-postgres.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
