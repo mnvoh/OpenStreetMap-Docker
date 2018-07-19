79bfd50259b43add257a1abca8d2a2001cc166f9
Milad Nozari <mnvoh90@gmail.com>
2018-07-19 07:32:00 +0430

    Move the import command outside the dockerfile

--- web/run.sh
+++ web/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+~/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
+
+if [ -f /data/import.osm.pbf ]; then
+  osm2pgsql -H postgres -P 5432 -d ${POSTGRES_DB} \
+  --username ${POSTGRES_USER} --password ${POSTGRES_PASS} --slim  -G \
+  --hstore \
+  --tag-transform-script ~/openstreetmap-carto/openstreetmap-carto.lua \ 
+  -C ${IMPORT_MEMORY_SIZE} --number-processes ${IMPORT_CPU_COUNT} \
+  -S ~/openstreetmap-carto/openstreetmap-carto.style \
+  /data/import.osm.pbf
+fi
+
 renderd -c /usr/local/etc/renderd.conf
+
 
