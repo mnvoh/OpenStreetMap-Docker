285b34f59d22697fd821f80d8cc46854d882e5fe
Milad Nozari <mnvoh90@gmail.com>
2018-07-19 08:17:20 +0430

    Add wait-for-postgres script correctly and set its permissions

--- web/run.sh
+++ web/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/${USERNAME}/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
+/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
 
 if [ -f /data/import.osm.pbf ]; then
   osm2pgsql -H postgres -P 5432 -d ${POSTGRES_DB} \
