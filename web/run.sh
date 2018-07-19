221bc53fd060c4732962ff4e868f6e0753107a66
Milad Nozari <mnvoh90@gmail.com>
2018-07-19 08:04:00 +0430

    Fix entrypoint issues

--- web/run.sh
+++ web/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-~/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
+/home/${USERNAME}/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
 
 if [ -f /data/import.osm.pbf ]; then
   osm2pgsql -H postgres -P 5432 -d ${POSTGRES_DB} \
   --username ${POSTGRES_USER} --password ${POSTGRES_PASS} --slim  -G \
   --hstore \
-  --tag-transform-script ~/openstreetmap-carto/openstreetmap-carto.lua \ 
+  --tag-transform-script /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.lua \ 
   -C ${IMPORT_MEMORY_SIZE} --number-processes ${IMPORT_CPU_COUNT} \
-  -S ~/openstreetmap-carto/openstreetmap-carto.style \
+  -S /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.style \
   /data/import.osm.pbf
 fi
 
