5e464056b9db2405fd9ed96e760287880a58a2fd
Milad Nozari <mnvoh90@gmail.com>
2018-07-19 11:06:40 +0430

    Remove password from wait-for-postgres

--- web/wait-for-postgres.sh
+++ web/wait-for-postgres.sh
@@ -5,10 +5,9 @@ set -e
 
 host="$1"
 user="$2"
-PGPASSWORD="$3"
 shift
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$user" -c '\q'; do
+until psql -h "$host" -U "$user" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
