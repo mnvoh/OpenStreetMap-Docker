135d5294316d3f62987eb0b8a42295bc86e49dfb
Milad Nozari <mnvoh90@gmail.com>
2018-07-20 23:09:35 +0430

    Update and fix the issues in the run scripts

--- web/run.sh
+++ web/run.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-/wait-for-postgres.sh postgres ${POSTGRES_USER} ${POSTGRES_PASS}
+/wait-for-postgres.sh
 
 if [ -f /data/import.osm.pbf ]; then
-  PGPASSWORD=${POSTGRES_PASS} osm2pgsql -H postgres -P 5432 -d ${POSTGRES_DB} \
-  --username ${POSTGRES_USER} --password ${POSTGRES_PASS} --slim  -G \
-  --hstore \
-  --tag-transform-script /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.lua \ 
-  -C ${IMPORT_MEMORY_SIZE} --number-processes ${IMPORT_CPU_COUNT} \
-  -S /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.style \
-  /data/import.osm.pbf
+  osm2pgsql -H postgres -P 5432 -d ${POSTGRES_DB} --username ${POSTGRES_USER} --slim  -G --hstore --tag-transform-script /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.lua -C ${IMPORT_MEMORY_SIZE} --number-processes ${IMPORT_CPU_COUNT} -S /home/${USERNAME}/openstreetmap-carto/openstreetmap-carto.style -r pbf /data/import.osm.pbf
 fi
 
-renderd -f -c /usr/local/etc/renderd.conf
-
-
+service apache2 start
+su - www-data -c "renderd -f -c /usr/local/etc/renderd.conf" -s /bin/sh
--- web/wait-for-postgres.sh
+++ web/wait-for-postgres.sh
@@ -3,11 +3,7 @@
 
 set -e
 
-host="$1"
-user="$2"
-shift
-
-until psql -h "$host" -U "$user" -c '\q'; do
+until psql -h "postgres" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
